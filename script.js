d0c25b607c9b4af0af6ceaca8cb4ac77a6f1bdc3
LizethCalzada <ramirezcalzadalizeth1dpv@cetis50cdmx.com>
2024-03-06 19:24:25 -0600

    Se subieron lo archivos

--- script.js
+++ script.js
@@ -1,40 +1,31 @@
-function suma(){
-    let num1 = Number(document.getElementById('n1').value);
-    let num2 = Number(document.getElementById('n2').value);
-    
-    let res = num1 + num2;
-    document.getElementById('res').value = res;
-
+// Calcular el área de un cuadrado
+function Cuadrado() {
+    var lado = parseFloat(document.getElementById("lado").value);
+    var area = lado * lado;
+    document.getElementById("res").value = area;
 }
 
-function Resta() {
-    let num1 = Number(document.getElementById('n1').value);
-    let num2 = Number(document.getElementById('n2').value);
-
-    let res = num1 - num2;
-    document.getElementById('res').value = res;
+// Calcular el área de un triángulo
+function Triangulo() {
     
+    var n1 = parseFloat(document.getElementById("n1").value);
+    var n2 = parseFloat(document.getElementById("n2").value);
+    var area = (n1 * n2) / 2;
+    document.getElementById("res").value = area;
 }
 
-function Multiplicacion(){
-    let num1 = Number(document.getElementById('n1').value);
-    let num2 = Number(document.getElementById('n2').value);
-
-    let res = num1 * num2;
-    
-    document.getElementById('res').value = res;
+// Calcular el área de un círculo
+function Circulo() {
+    var radio = parseFloat(document.getElementById("radio").value);
+    var area = Math.PI * radio * radio;
+    area = area.toFixed(2);
+    document.getElementById("res").value = area;
 }
 
-function Division (){
-    let num1 = Number(document.getElementById('n1').value);
-    let num2 = Number(document.getElementById('n2').value);
-
-    if (num2 !== 0){
-
-        let res = num1 / num2;
-        document.getElementById('res').value = res;
-    
-    }else{
-        alert("No se puede dividir por cero.");
-    }
+// Calcular el área de un rectángulo
+function Rectangulo() {
+    var longitud = parseFloat(document.getElementById("longitud").value);
+    var anchura = parseFloat(document.getElementById("anchura").value);
+    var area = longitud * anchura;
+    document.getElementById("res").value = area;
 }
